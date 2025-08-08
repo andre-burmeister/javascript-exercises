297e65b8025b5492938f3675c30a2ec391a2f50b
André Burmeister <andre.b.burmeister@gmail.com>
2025-08-08 15:02:17 -0300

    Solve exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+  if (n < 0) {
+    return "ERROR"
+  }
+  let returnStr = ""
+  for (let i=0; i<n; i++) {
+    returnStr = returnStr + str
+  }
+  return returnStr
 };
 
 // Do not edit below this line
